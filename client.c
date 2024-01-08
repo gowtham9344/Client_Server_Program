66e9d79017bdb16a6d041add50ae1de8b3d13561
gowtham9344 <gowtham@mobicip.com>
2024-01-08 15:53:12 +0530

    added comments and modularized the code

--- client.c
+++ client.c
@@ -10,30 +10,26 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
  
-#define PORT "8050" 
+#define PORT "3490" 
 #define MAX 100
 #define SA struct sockaddr
 
+// give IPV4 or IPV6  based on the family set in the sa
 void *get_in_addr(struct sockaddr *sa){
 	if(sa->sa_family == AF_INET){
 		return &(((struct sockaddr_in*)sa)->sin_addr);	
 	}
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
- 
-int main(int argc,char* argv[]){ 
-	int sockfd,numbytes; 	
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-	char buff[100];
-	printf("Enter the message needs to be send to server:");
-	scanf("%s",buff);
 
-	if(argc != 2){
-		fprintf(stderr,"usage: client hostname\n");
-		exit(1);	
-	}
+// this is the code for client creation. here i have used TCP instead of UDP because i need all the data without any loss. if we use UDP we
+// have to implement those in the upper layers.
+// this function will return socket descripter to the calling function.
+int client_creation(int argc, char* argv[]){
+	int sockfd;
+	struct addrinfo hints,*servinfo,*p;
+	int rv;
+
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
@@ -50,6 +46,7 @@ int main(int argc,char* argv[]){
 			continue; 
 		}
 		
+		// connect will help us to connect to the server with the addr given in arguments.
 		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(sockfd);
 			perror("client: connect");
@@ -63,17 +60,22 @@ int main(int argc,char* argv[]){
 		return 2;	
 	}
 	
+	//printing ip address of the server.
 	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),s, sizeof(s));
 	printf("client: connecting to %s\n", s);
-	
 	freeaddrinfo(servinfo);
-		
 	
+	return sockfd;
+}
+
+void message_handler(int sockfd){
+	// sending message to the server.
 	if (send(sockfd,buff,sizeof(buff),0) == -1){ 
 		perror("send");
 		exit(1); 
 	} 
 
+	//receving message from the server.
 	if(recv(sockfd,buff,sizeof(buff),0) == -1){ 
 		perror("recv");
 		exit(1); 
@@ -86,7 +88,25 @@ int main(int argc,char* argv[]){
 	strncpy(buff1,ctime(&rawtime),strlen(ctime(&rawtime))-1);
 	sprintf(buff,"%s %s",buff,buff1);
 	printf("The final received message with the time stamp is \n%s\n",buff); 
-	close(sockfd); 
+}
+ 
+int main(int argc,char* argv[]){ 
+	int sockfd; 
+	char buff[100];
+	printf("Enter the message needs to be send to server:");
+	scanf("%s",buff);
+
+	// should provide the server ip address when running in the arguments.
+	if(argc != 2){
+		fprintf(stderr,"usage: client hostname\n");
+		exit(1);	
+	}
+	
+	sockfd = client_creation(argc,argv);
+		
+	message_handler(sockfd);
+	
+	close(sockfd);
 	return 0;
 }
 
--- server.c
+++ server.c
@@ -15,14 +15,19 @@
 
 #define SA struct sockaddr 
 #define BACKLOG 10 
-#define PORT "8050" 
+#define PORT "3490" 
 
+
+
+//it helps us to handle all the dead process which was created with the fork system call.
 void sigchld_handler(int s){
 	int saved_errno = errno;
 	while(waitpid(-1,NULL,WNOHANG) > 0);
 	errno = saved_errno;
 }
 
+
+// give IPV4 or IPV6  based on the family set in the sa
 void *get_in_addr(struct sockaddr *sa){
 	if(sa->sa_family == AF_INET){
 		return &(((struct sockaddr_in*)sa)->sin_addr);	
@@ -31,27 +36,26 @@ void *get_in_addr(struct sockaddr *sa){
 }
 
 
-int main(){ 
-	int sockfd,connfd; 
+// this is the code for server creation. here i have used TCP instead of UDP because i need all the data without any loss. if we use UDP we
+// have to implement those in the upper layers.
+// this function will return socket descripter to the calling function.
+int server_creation(){
+	int sockfd;
 	struct addrinfo hints,*servinfo,*p;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
-	struct sigaction sa;
 	int yes = 1;
-	char s[INET6_ADDRSTRLEN];
 	int rv;
-
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;// my ip
 	
+	// set the address of the server with the port info.
 	if((rv = getaddrinfo(NULL,PORT,&hints,&servinfo)) != 0){
 		fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(rv));	
 		return 1;
 	}
 	
-	// loop through all the results and bind to the first we can
+	// loop through all the results and bind to the socket in the first we can
 	for(p = servinfo; p!= NULL; p=p->ai_next){
 		sockfd=socket(p->ai_family,p->ai_socktype,p->ai_protocol);
 		if(sockfd==-1){ 
@@ -59,11 +63,15 @@ int main(){
 			continue; 
 		} 
 		
+		// SO_REUSEADDR is used to reuse the same port even if it was already created by this.
+		// this is needed when the program is closed due to some system errors then socket will be closed automaticlly after few
+		// minutes in that case before the socket is closed if we rerun the program then we have use the already used port 	
 		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
 			perror("setsockopt");
 			exit(1);	
 		}
 		
+		// it will help us to bind to the port.
 		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(sockfd);
 			perror("server: bind");
@@ -78,53 +86,101 @@ int main(){
 	}
 	
 
+	// server will be listening with maximum simultaneos connections of BACKLOG
 	if(listen(sockfd,BACKLOG) == -1){ 
 		perror("listen");
 		exit(1); 
 	} 
-	sa.sa_handler = sigchld_handler; // reap all dead processes
+	return sockfd;
+}
+
+//connection establishment with the client
+//return connection descriptor to the calling function
+int connection_accepting(int sockfd){
+	int connfd;
+	struct sockaddr_storage their_addr;
+	char s[INET6_ADDRSTRLEN];
+	socklen_t sin_size;
+	
+	sin_size = sizeof(their_addr); 
+	connfd=accept(sockfd,(SA*)&their_addr,&sin_size); 
+	if(connfd == -1){ 
+		perror("\naccept error\n");
+		return -1;
+	} 
+	//printing the client name
+	inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof(s));
+	printf("\nserver: got connection from %s\n", s);
+	
+	return connfd;
+}
+
+void message_handler(int connfd){
+	char buff[100];
+	int n;
+	//receving data from the client
+	if((n = recv(connfd,buff,sizeof(buff),0)) == -1){
+		printf("msg not received\n"); 
+		exit(0); 
+	} 
+	buff[n] = '\0';
+	//received timestamp of the message
+	time_t rawtime=0; 
+	rawtime=time(NULL);
+	char buff1[100];
+			
+	// taking only the time without new line character
+	strncpy(buff1,ctime(&rawtime),strlen(ctime(&rawtime))-1);
+	buff1[strlen(ctime(&rawtime))-1] = '\0';
+	int w;
+	printf("wait for sometime and enter any number for time difference delay:");
+	scanf("%d",&w); 
+	sprintf(buff1,"%s %s",buff1,buff);
+	
+	//sending data to the client		 
+	if (send(connfd,buff1,sizeof(buff1),0) == -1){ 
+		printf("not send\n"); 
+		exit(0); 
+	} 
+}
+
+
+int main(){ 
+	int sockfd,connfd; 
+	struct sigaction sa;
+	
+
+	//server creation .
+	sockfd = server_creation();
+
+	// reap all dead processes that are created as child processes
+	sa.sa_handler = sigchld_handler; 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
 		perror("sigaction");
 		exit(1);
 	}
+
 	printf("server: waiting for connections...\n");
 	 
 	while(1){ 
-		sin_size = sizeof(their_addr); 
-		connfd=accept(sockfd,(SA*)&their_addr,&sin_size); 
-		if(connfd == -1){ 
-			perror("\naccept error\n");
+		
+		connfd = connection_accepting(sockfd);
+			
+		if(connfd == -1){
 			continue;
-		} 
-		inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof(s));
-		printf("\nserver: got connection from %s\n", s);
-  
+		}
+
+		// fork is used for concurrent server.
+		// here fork is used to create child process to handle single client connection because if two clients needs to 
+		// connect to the server simultaneously if we do the client acceptence without fork if some client got connected then until 
+		// the client releases the server no one can able to connect to the server.
+		// to avoid this , used fork, that creates child process to handle the connection.
 		int fk=fork(); 
 		if (fk==0){ 
 			close(sockfd);
-			char buff[100];
-			if(recv(connfd,buff,sizeof(buff),0) == -1){
-				printf("msg not received\n"); 
-				exit(0); 
-			} 
-
-			//received timestamp of the message
-			time_t rawtime=0; 
-			rawtime=time(NULL);
-			char buff1[100];
-
-			// taking only the time without new line character
-			strncpy(buff1,ctime(&rawtime),strlen(ctime(&rawtime))-1);
-			int w;
-			printf("wait for sometime and enter any number for time difference delay:");
-			scanf("%d",&w); 
-			sprintf(buff1,"%s %s",buff1,buff);		 
-			if (send(connfd,buff1,sizeof(buff1),0) == -1){ 
-				printf("not send\n"); 
-				exit(0); 
-			} 
+			message_handler(connfd);			
 			close(connfd); 
 			exit(0);
 		} 
